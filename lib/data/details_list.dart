3434d3e1ef27adb6897f89b246cdca608175907f
Anju6770 <anjalighalleyrai@gmail.com>
2024-08-20 18:03:01 +0600

    Initial commit

---
+++ lib/data/details_list.dart
@@ -0,0 +1,84 @@
+import 'package:beauty/model/details.dart';
+
+List<Detail1> products=[
+  Detail1(
+      image: "assets/products/Concealer_1-removebg-preview.png",
+      name: "Instant Eraser",
+      payment: "\$ 10",
+      brife: "Instant Eraser is the do-it-all concealer that moisturizes too! This multi-use concealer does it all: hydrates, conceals, contours and corrects in a click!",
+      name2: 'Concealer',
+    quantity: 1,
+    rating: '',
+  ),
+  Detail1(
+      image: "assets/products/make_up_forever-removebg-preview.png",
+      name: "Forever",
+      payment: "\$ 15",
+      brife: "While there’s no doubt that naturally poreless skin and no-makeup selfies will forever be the ultimate goal, sometimes you could use a little help enhancing your complexion.",
+      name2: 'Foundation',
+    quantity: 1,
+    rating: '',
+  ),
+  Detail1(
+      image: "assets/products/17_Best_Eyeshadow_Palettes-removebg-preview.png",
+      name: "Palettes",
+      payment: "\$ 18",
+      brife: "It contains various neutral, cool, and warm nude shades as well as some subtle pops of color that you can get creative with.",
+      name2: 'Eyeshadow',
+    quantity: 1,
+    rating: '',
+  ),
+  Detail1(
+      image: "assets/products/blush-removebg-preview.png",
+      name: "Blusher",
+      payment: "\$ 6",
+      brife: ". Comes in a beautiful Peach Bliss Naturally Sun kissed Pink shade that suits all skin tones, perfect for creating a fresh and youthful-looking complexion.",
+      name2: 'Blush',
+    quantity: 1,
+    rating: '',
+  ),
+  Detail1(
+      image: "assets/products/lipstick-removebg-preview.png",
+      name: "Mette Lipstick",
+      payment: "\$ 13",
+      brife: "A silky matte lipstick that delivers 12 hours of full-coverage colour and eight hours of moisture.",
+      name2: 'Lipstick',
+    quantity: 1,
+    rating: '',
+  ),
+];
+
+List<Detail2> lip=[
+  Detail2(
+      image: "assets/images/lip1-removebg-preview.png",
+      name: "Charlotte Tilbury ",
+      payment: "\$ 17",
+      brife: "brife",
+      quen: "200ml"
+  ),Detail2(
+      image: "assets/images/lip2-removebg-preview.png",
+      name: "M.A.C Matte Nude",
+      payment: "\$ 18",
+      brife: "brife",
+      quen: "200ml"
+  ),Detail2(
+      image: "assets/images/lip3-removebg-preview.png",
+      name: "Huda Beauty",
+      payment: "\$ 20",
+      brife: "brife",
+      quen: "200ml"
+  ),Detail2(
+      image: "assets/images/lip4-removebg-preview.png",
+      name: "Faces Canada",
+      payment: "\$ 21",
+      brife: "brife",
+      quen: "200ml"
+  ),Detail2(
+      image: "assets/images/lip5-removebg-preview.png",
+      name: "Lakmé Absolute",
+      payment: "\$ 19",
+      brife: "brife",
+      quen: "200ml"
+  ),
+
+];
